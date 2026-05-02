bb837141d9adee3fd1499ebe4f96ed45c10a44e3
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2026-05-02 07:18:29 +0100

    Initial Contribution #2
    https://github.com/regcommunity/bird-default-test-suite-ldm-67/issues/2

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+Default Test Suite Test Suite
+Default Test Suite test suite for BIRD Bench
+"""
+
+__version__ = "1.0.0"
+__author__ = "Test Suite Developer"
---
+++ tests/code/test_cell_f_05_01_ref_finrep_3_0_152457_ref__loan_and_guarantee_scenario_2.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import logging
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'birds_nest.settings'
+from django.conf import settings
+import pytest
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from pybirdai.entry_points.execute_datapoint import RunExecuteDataPoint
+from pybirdai.process_steps.pybird.execute_datapoint import ExecuteDataPoint
+from pybirdai.process_steps.filter_code.report_cells import Cell_F_05_01_REF_FINREP_3_0_152457_REF
+
+def test_execute_datapoint(value: int=11):
+    data_point_id = 'F_05_01_REF_FINREP_3_0_152457_REF'
+    result = RunExecuteDataPoint.run_execute_data_point(data_point_id)
+    ExecuteDataPoint.delete_lineage_data()
+    assert result == str(value)
