284f0e3799c5e79aa69bacb6b71ddb101b878e59
RBirdwatcher <neil@birdsoftwaresolutions.com>
2026-08-02 16:10:47 +0100

    updated_test_data

--- tests/code/test_cell_f_05_01_ref_finrep_3_0_152589_ref__loan_and_guarantee_scenario_1.py
+++ tests/code/test_cell_f_05_01_ref_finrep_3_0_152589_ref__loan_and_guarantee_scenario_1.py
@@ -1,9 +1,5 @@
 import os
-import sys
 import logging
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
-if PROJECT_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'birds_nest.settings'
 from django.conf import settings
 import pytest
@@ -13,7 +9,7 @@ from pybirdai.entry_points.execute_datapoint import RunExecuteDataPoint
 from pybirdai.process_steps.pybird.execute_datapoint import ExecuteDataPoint
 from pybirdai.process_steps.filter_code.report_cells import Cell_F_05_01_REF_FINREP_3_0_152589_REF
 
-def test_execute_datapoint(value: int=83491261):
+def test_execute_datapoint(value: int=83491250):
     data_point_id = 'F_05_01_REF_FINREP_3_0_152589_REF'
     result = RunExecuteDataPoint.run_execute_data_point(data_point_id)
     ExecuteDataPoint.delete_lineage_data()
